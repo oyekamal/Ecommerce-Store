029b37e7150b3f46771fa6b5925886370b7d8071
kamalrapi <muhammad.kamal@rapidev.ae>
2021-04-27 11:00:52 +0500

    detail product done

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import  User
+from django.urls import  reverse
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
@@ -9,8 +10,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
-    # def get_absolute_url(self):
-    #     return reverse("store:category_list", args=[self.slug])
+    def get_absolute_url(self):
+        return reverse("store:category_list", args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -35,4 +36,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
     
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 # Create your views here.
 
@@ -11,3 +11,7 @@ def category(request):
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html',{'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, "store/products/details.html", {"product":product})
